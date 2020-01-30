09889179d89b14d7431d873acc0e4d368c8d7e2d
dayo4 <dayo4@live.com>
2020-01-30 17:25:27 +0100

    nothing

--- src/app.js
+++ src/app.js
@@ -8,27 +8,17 @@ class App {
 
     constructor () {
         //Ajuwaya Function Inits
-<<<<<<< HEAD
-        this.Init()
-        console.log("Ajuwaya Module is working now")
-=======
+
         this.Init();
         console.log("Ajuwaya working...");
->>>>>>> origin/ajuwaya-module
     }
 
     /*****************************************************************************
                             INITIALIZE Ajuwaya FUNCTION
     *****************************************************************************/
-<<<<<<< HEAD
-    Init() {
-        //Login Function
-        Login()
 
-=======
     Init(){
         AjuwayaSeperator()
->>>>>>> origin/ajuwaya-module
     }
 
     /*****************************************************************************
@@ -40,12 +30,7 @@ class App {
 
 }
 
-<<<<<<< HEAD
-let app = new App()
 
-=======
 let app = new App();
->>>>>>> origin/ajuwaya-module
 //ReactDOM.render(<App />, document.getElementById('root'));
 
-//
