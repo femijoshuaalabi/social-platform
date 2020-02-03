776cc2f0417c18bd0321a6242d51fc287b75125d
AjuwayaConnect <corpersmeet@gmail.com>
2020-02-03 16:50:20 +0100

    Lastest

--- src/components/Message/Components/ConversationLists.js
+++ src/components/Message/Components/ConversationLists.js
@@ -5,6 +5,8 @@ import { ConversationReplies } from './ConversationReplies'
 
 import { Networks } from '../../Networks/Networks'
 
+import { TimeConverter } from '../../Functionalities'
+
 /*****************************************************************************
                         DISPLAY USER CONTACT LIST
 *****************************************************************************/
@@ -31,7 +33,7 @@ export function ConversationLists() {
         {
 
             $.each(result.conversations, (i, data) => {
-
+                console.log(data)
                 const conversationsList = `
                         <div id="msgList" key="${ data.uid }" class="row no-gutters flex-nowrap align-items-center p-1">
                             <div>
@@ -40,7 +42,7 @@ export function ConversationLists() {
                             <div class="py-1 pl-2 flex-grow-1" style="max-width:85%;">
                                 <div class="row no-gutters flex-nowrap justify-content-between align-items-center">
                                     <h6 class="mb-1 font-weight-bolder text-truncate" style="max-width:60%;">${ data.name }</h6>
-                                    <p class="mb-1 text-right mr-3 text-nowrap"><span class="small "><small>Jan 23, 02:25PM</small></span></p>
+                                    <p class="mb-1 text-right mr-3 text-nowrap"><span class="small "><small>${TimeConverter(data.time)}</small></span></p>
                                 </div>
                                 <p class="small my-0 text-muted text-truncate pr-2">
                                 ${ data.lastReply.reply }
